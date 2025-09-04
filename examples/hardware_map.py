99b5a02929f325001b36921dad56d08f4c27d030
TosakaRin <isaacc@mail.ustc.edu.cn>
2025-09-04 22:38:22 +0800

    Add example for debugging

--- examples/hardware_map.py
+++ examples/hardware_map.py
@@ -13,4 +13,8 @@ rwg4 = Board(id="rwg4")
 mot_repump = Channel(board=rwg0, local_id=0, channel_type=ChannelType.RWG)
 mot_cooling = Channel(board=rwg0, local_id=1, channel_type=ChannelType.RWG)
 cooling_lock = Channel(board=rwg4, local_id=0, channel_type=ChannelType.RWG)
+global_imaging = Channel(board=rwg0, local_id=2, channel_type=ChannelType.RWG)
+global_repump = Channel(board=rwg0, local_id=3, channel_type=ChannelType.RWG)
+
+uv_led = Channel(board=rwg0, local_id=0, channel_type=ChannelType.TTL)
 

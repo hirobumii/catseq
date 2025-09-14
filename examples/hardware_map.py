340f32c60fb4f3de285f0f395b584a348f1dab63
hirobumi <isaacc@mail.ustc.edu.cn>
2025-09-14 23:40:13 +0800

    add some hardware maps;

--- examples/hardware_map.py
+++ examples/hardware_map.py
@@ -6,6 +6,7 @@ from catseq.types.common import Board, Channel, ChannelType
 main = Board(id="main")
 rwg0 = Board(id="rwg0")
 rwg1 = Board(id="rwg1")
+rwg2 = Board(id="rwg2")
 rwg4 = Board(id="rwg4")
 
 # 2. Define specific channels on the boards
@@ -22,3 +23,17 @@ artiq_trig = Channel(board=rwg1, local_id=0, channel_type=ChannelType.TTL)
 gradient_mag = Channel(board=rwg0, local_id=0, channel_type=ChannelType.TTL)
 mag_trig = Channel(board=rwg0, local_id=1, channel_type=ChannelType.TTL)
 qcmos_trig = Channel(board=rwg0, local_id=2, channel_type=ChannelType.TTL)
+
+blowoff = Channel(board=rwg1, local_id=0, channel_type=ChannelType.RWG)
+eit1 = Channel(board=rwg1, local_id=1, channel_type=ChannelType.RWG)
+eit2 = Channel(board=rwg1, local_id=2, channel_type=ChannelType.RWG)
+local_repump = Channel(board=rwg1, local_id=3, channel_type=ChannelType.RWG)
+
+local_image1 = Channel(board=rwg2, local_id=0, channel_type=ChannelType.RWG)
+local_image2 = Channel(board=rwg2, local_id=1, channel_type=ChannelType.RWG)
+local_image_total = Channel(board=rwg2, local_id=2, channel_type=ChannelType.RWG)
+
+cooling_shutter = Channel(board=rwg2, local_id=0, channel_type=ChannelType.TTL)
+mot_repump_shutter = Channel(board=rwg2, local_id=1, channel_type=ChannelType.TTL)
+global_imaging_shutter = Channel(board=rwg2, local_id=2, channel_type=ChannelType.TTL)
+blowoff_shutter = Channel(board=rwg2, local_id=3, channel_type=ChannelType.TTL)

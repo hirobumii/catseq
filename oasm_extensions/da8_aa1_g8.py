d7e012ad8378ea211620abc06191620c68a71315
hirobumi <isaacc@mail.ustc.edu.cn>
2025-11-12 23:51:05 +0800

    update da8_aa1_g8.py to fit the new firmware

--- oasm_extensions/da8_aa1_g8.py
+++ oasm_extensions/da8_aa1_g8.py
@@ -3,6 +3,7 @@ from .flex import *
 class da8_aa1_g8(flex.__class__):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.config(1,12,6,1)
+        #self.config(1,12,6,1)
+        self.config(8,12,6,16,4)
     
 da8_aa1_g8 = da8_aa1_g8(**globals())
